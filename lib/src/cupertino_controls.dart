88f68ac9ba22a504a5e2ecd1555616fdf4239ffd
djc <703876992@qq.com>
2019-07-22 21:22:54 +0800

    annotation code

--- lib/src/cupertino_controls.dart
+++ lib/src/cupertino_controls.dart
@@ -141,15 +141,14 @@ class _CupertinoControlsState extends State<CupertinoControls> {
                     )
                   : Row(
                       children: <Widget>[
-                        _buildSkipBack(iconColor, barHeight),
+                        // _buildSkipBack(iconColor, barHeight),
                         _buildPlayPause(controller, iconColor, barHeight),
-                        _buildSkipForward(iconColor, barHeight),
+                        // _buildSkipForward(iconColor, barHeight),
                         _buildPosition(iconColor),
                         _buildProgressBar(),
                         _buildRemaining(iconColor)
                       ],
                     ),
-
             ),
           ),
         ),
